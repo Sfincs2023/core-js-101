7c56408cc37085955e5632d4cb252e5e2bd5e349
Sfincs2023 <Naumenko.Aleksey@gmail.com>
2025-09-01 16:55:38 -0400

    fix: delete test file

--- xxx.js
+++
@@ -1,9 +0,0 @@
-function getDigitalRoot(num) {
-  let strNum = num.toString();
-  while (strNum.length > 1) {
-    strNum = strNum.split('').reduce((sum, n) => Number(sum) + Number(n), 0).toString();
-  }
-  return +strNum;
-}
-
-console.log(getDigitalRoot(235));
